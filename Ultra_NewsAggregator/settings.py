d30bcc97bf9483718b0196e402a0ed1e7a9dd5bb
“Emmanuel <emma.moghalu@gmail.com>
2024-04-25 13:54:36 +0100

    hosts update

--- Ultra_NewsAggregator/settings.py
+++ Ultra_NewsAggregator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$*-qu)4o_a59bls@(xtd^_%=yu-t4c7=9hynve-=p)l9m$)#d-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1','ultra-news-emc.koyeb.app']
 
 
 # Application definition
